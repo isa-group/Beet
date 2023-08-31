c6031a49064fc72833cab9509f080019c39b066c
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2023-08-31 09:21:39 -0700

    Generate set values instrumentation

--- src/main/java/agora/beet/main/GenerateInstrumentation.java
+++ src/main/java/agora/beet/main/GenerateInstrumentation.java
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
 public class GenerateInstrumentation {
 
     private static String openApiSpecPath = "src/test/resources/pr_test/swagger_albumTracks.yaml";
-    private static String testCasesFilePath = "src/test/resources/pr_test/Spotify_GetAlbumTracks_50.csv";
+    private static String testCasesFilePath = "src/test/resources/pr_test/setValues/Spotify_GetAlbumTracks_setValues.csv";
     private static boolean generateDtrace = true;
     public static String[] stringsToConsiderAsNull = {};
 
