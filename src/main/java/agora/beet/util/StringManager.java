c423b202de4e7dda665fa3dae5b4ec5a76fd4f35
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-12-19 01:52:11 +0100

    Implement parameter name decoding

--- src/main/java/agora/beet/util/StringManager.java
+++ src/main/java/agora/beet/util/StringManager.java
@@ -2,6 +2,8 @@ package agora.beet.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author Juan C. Alonso
@@ -9,18 +11,31 @@ import java.net.URLDecoder;
 public class StringManager {
 
     /**
+     * Used to encode URL special characters (e.g., convert image[0] to images%5B0%5D in Stripe API)
+     * @param parameterName Name of the input parameter we want to encode
+     * @return Encoded string
+     */
+    public static String encodeString(String parameterName) {
+
+        if (parameterName == null) {
+            throw new IllegalArgumentException("parameterValue cannot be null");
+        }
+
+        return URLEncoder.encode(parameterName, StandardCharsets.UTF_8);
+    }
+
+    /**
      * Used to decode URL special characters (e.g., replace "+" with " ")
      * @param parameterValue: Encoded URL string
      * @return Decoded URL string
      */
     public static String decodeString(String parameterValue) {
-        try {
-            return URLDecoder.decode(parameterValue, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            System.exit(1);
+
+        if (parameterValue == null) {
+            throw new IllegalArgumentException("parameterValue cannot be null");
         }
-        return null;
+
+        return URLDecoder.decode(parameterValue, StandardCharsets.UTF_8);
     }
 
 }
